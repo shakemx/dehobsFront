a929caa9be6a64f1a4b404758e57d2cb5c1bacf9
Guillermo Pineda <guillermopineda@Mac-mini-de-Guillermo.local>
2022-03-31 22:07:16 -0600

    Casos de Exito

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,3 +13,32 @@ btn.on('click', function(e) {
   $('html, body').animate({scrollTop:0}, '300');
 });
 
+
+
+// $(document).ready(function() {
+//   $('#myCarousel').carousel({
+//      interval: 3000,
+//       pause: false
+//   });
+// });
+
+// var pol = $('#prueba');
+// pol.on('click', function(e) {
+//   e.preventDefault();
+//   document.getElementById('cinte').style.transform = 'translate3d(-25%, 0, 0)'
+
+// });
+
+// var pol = $('#prueba');
+// var ele = document.getElementById('cinte');
+// pol.on('click', function(e) {
+//   e.preventDefault();
+//   ele.animate({transform: 'translate3d(-25%, 0, 0)' }, 1000, function() {
+//     if(Math.abs(ele.position) === 0){
+//       ele.css({transform: 'translate3d(-25%, 0, 0)'});
+//     }
+//   });
+//   console.log(ele.transform);
+
+
+// });
