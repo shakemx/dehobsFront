a96216acad78ccf89f99cb3a79f56336ba1392b1
Guillermo Pineda <guillermopineda@Mac-mini-de-Guillermo.local>
2022-04-01 11:01:44 -0600

    formularip

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -42,3 +42,80 @@ btn.on('click', function(e) {
 
 
 // });
+
+
+
+(function () {
+  emailjs.init("user_gGHZQEDHJmDEjuOwEPoep");
+})();
+
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName("needs-validation");
+      var myModalGracias = new bootstrap.Modal(
+        document.getElementById("modalGracias"),
+        {}
+      );
+      var myModalError = new bootstrap.Modal(
+        document.getElementById("modalError"),
+        {}
+      );
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            event.preventDefault();
+            try {
+              var template_params = {
+                nombre_contacto: document.getElementById("nombre_contacto").value,
+                correo_contacto: document.getElementById("correo_contacto").value,
+                telefono_contacto:
+                  document.getElementById("telefono_contacto").value,
+                asunto_contacto: document.getElementById("asunto_contacto").value,
+                mensaje_contacto:
+                  document.getElementById("mensaje_contacto").value,
+                medio_contacto: document.getElementById("medio_contacto").value,  
+                'g-recaptcha-response': ' 6Ldg9zcfAAAAAJL97akoN2nLnOw-Ye4VbIMWo3Yq ',        
+              };
+              emailjs.send(
+                "service_g8uwlxg",
+                "template_7n0pmcc",
+                template_params
+              );
+              myModalGracias.show();
+              setTimeout(function () {
+                myModalGracias.hide();
+              }, 3000);
+            } catch (error) {
+              myModalError.show();
+              setTimeout(function () {
+                myModalError.hide();
+              }, 3000);
+              console.log(error);
+            }
+            nombre_contacto.value = "";
+            correo_contacto.value = "";
+            telefono_contacto.value = "";
+            asunto_contacto.value = "";
+            mensaje_contacto.value = "";
+            medio_contacto.value = "";
+            grecaptcha.reset();
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
+
+
